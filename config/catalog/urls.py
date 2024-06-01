8f57826239c69c631e2ed5377a4316bd6d3964fd
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-06-01 21:08:30 +0300

    'Обновление URL-адреса для поддержки всех функций

--- config/catalog/urls.py
+++ config/catalog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('contacts/', views.contact, name='contact'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('create/', views.create_product, name='create_product'),
 ]
