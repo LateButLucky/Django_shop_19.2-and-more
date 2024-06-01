8db314a21981ae3cce306f54b1be1a9d62428951
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-06-01 21:08:04 +0300

    'Добавление функционала создания продукта через интерфейс и создание соответствующего шаблона

--- config/catalog/forms.py
+++ config/catalog/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from .models import Product
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=20)
     message = forms.CharField(label='Сообщение', widget=forms.Textarea)
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'category', 'price']
