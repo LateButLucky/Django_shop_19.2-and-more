1873c6dd45e17c1fab698aa95d30c1576b5e3ae8
Aleksandr Podlesnov <sawawawre@gmail.com>
2024-06-01 21:05:55 +0300

    'Создание нового контроллера и шаблона для отображения страницы продукта, создание базового шаблон и подшаблона с главным меню, подключение их к другим шаблонам'

--- config/catalog/views.py
+++ config/catalog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
-from .forms import ContactForm
+from django.core.paginator import Paginator
+from .forms import ContactForm, ProductForm
 from .models import Contact, Product, Category
 
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    paginator = Paginator(products, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/home.html', {'page_obj': page_obj})
 
 
 def index(request):
@@ -25,3 +30,20 @@ def contact(request):
         message = request.POST.get('message')
         Contact.objects.create(name=name, phone=phone, message=message)
     return render(request, 'contact.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/create_product.html', {'form': form})
